cf769902b9d6440befce6f0dea3fa8ce24be2a46
Nonnadda <>
2024-07-09 20:49:12 +0300

    2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Part = ({part}) => {
+  return <p>{part.name} {part.exercises}</p>
+}
+const Content = ({parts}) => {
+  return <div>
+    {
+      parts.map(part=><Part key={part.id} part={part}/>)
+    }
+  </div>
+}
+
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Total = ({parts}) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return <p>Number of exercises {total}</p>
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      {/* <Total parts={course.parts}/> */}
+    </div>
+  )
+} 
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
