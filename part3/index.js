8808c7a57181e6f69bbb4ef9dbcab81762658242
Nonnadda <>
2024-07-10 20:10:38 +0300

    3.4

--- part3/index.js
+++ part3/index.js
@@ -41,6 +41,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
   response.send(`
     <div>
