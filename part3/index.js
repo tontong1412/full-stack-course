3c46c6ec6fab80bb27ff516cbe13b6ebb001d63c
Nonnadda <>
2024-07-14 17:00:07 +0300

    3.15-3.16

--- part3/index.js
+++ part3/index.js
@@ -17,9 +17,16 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  PhoneBook.findById(id).then(person => response.json(person))
+  PhoneBook.findById(id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
@@ -40,9 +47,10 @@ app.post('/api/persons', (request, response) => {
   newPerson.save().then(savedPerson => response.json(savedPerson))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   PhoneBook.findByIdAndDelete(id).then(person => response.status(204).json(person))
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -53,6 +61,23 @@ app.get('/info', (request, response) => {
     </div>`
   )
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
