e0405672180a7340df36cfee1c8bb06ca1e2d6d8
Nonnadda <>
2024-07-14 23:05:32 +0300

    3.20

--- part3/models/PhoneBook.js
+++ part3/models/PhoneBook.js
@@ -20,7 +20,16 @@ const phoneBookSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  },
 })
 
 phoneBookSchema.set('toJSON', {
