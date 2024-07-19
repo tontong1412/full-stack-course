a3a32ba6edd8eac8a601f7ad7bb0d8c5a6f7cec6
Nonnadda Silamai <nonnadda.silamai@aalto.fi>
2024-07-19 10:25:34 +0300

    4.4

---
+++ part4/tests/list_helper.test.js
@@ -0,0 +1,59 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const biggerList = [
+    ...listWithOneBlog,
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.equal(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.equal(result, 5)
+  })
+
+  test('of a bigger list is calcaulated right', () => {
+    const result = listHelper.totalLikes(biggerList)
+    assert.equal(result, 27)
+  })
+})
+
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((prev, curr) => (prev += curr.likes), 0)
 }
 
 module.exports = {
-    dummy
+  dummy,
+  totalLikes
 }
