11be1986c303bfa2f53a05277c060a8d6432aec9
Nonnadda <>
2024-07-21 00:24:02 +0300

    5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [viewDetail, setViewDetail] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+  <div style={blogStyle}>
+    {blog.title}
+    <button onClick={()=>setViewDetail(!viewDetail)}>{viewDetail? 'hide':'view'}</button>
+    {
+      viewDetail&&(
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.author}</div>
+        </div>
+      )
+    }
+    
   </div>  
-)
+  )
+}
 
 export default Blog
